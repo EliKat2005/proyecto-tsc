c987685993e8bf44b5d1587a2946bf3278c0f028
elikat <205855200+EliKat2005@users.noreply.github.com>
2025-10-14 11:43:05 -0500

    Archivo Base

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function greeter(name) {
+    return `Hola, ${name}! — desde TypeScript y Node ${process.version}`;
+}
+console.log(greeter("Elian"));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+function greeter(name: string) {
+  return `Hola, ${name}! — desde TypeScript y Node ${process.version}`;
+}
+console.log(greeter("Elikat"));
