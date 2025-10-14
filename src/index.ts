854e9db455656eca61f6333e7d2a1437b13e4f66
elikat <205855200+EliKat2005@users.noreply.github.com>
2025-10-14 13:19:14 -0500

    Funcion de Notas implementada

--- src/index.ts
+++ src/index.ts
@@ -23,9 +23,45 @@ function preguntarDatos() {
   });
 }
 
+function promedioNotas(){
+  rl.question('¿Cuántas notas deseas ingresar? ', (cantidad) => {
+    const numNotas = parseInt(cantidad);
+    if (isNaN(numNotas) || numNotas <= 0) {
+      console.log('Por favor, ingresa un número válido mayor que cero.');
+      rl.close();
+      return;
+    }
 
+    let notas: number[] = [];
+    let contador = 0;
 
-function greeter(name: string) {
-  return `Hola, ${name}! — desde TypeScript y Node ${process.version}`;
+    const preguntarNota = () => {
+      if (contador < numNotas) {
+        rl.question(`Ingresa la nota ${contador + 1}: `, (nota) => {
+          const notaNumero = parseFloat(nota);
+          if (isNaN(notaNumero) || notaNumero < 0 || notaNumero > 10) {
+            console.log('Por favor, ingresa una nota válida entre 0 y 10.');
+          } else {
+            notas.push(notaNumero);
+            contador++;
+          }
+          preguntarNota();
+        });
+      } else {
+        const suma = notas.reduce((a, b) => a + b, 0);
+        const promedio = suma / notas.length;
+        console.log(`El promedio de las notas es: ${promedio.toFixed(2)}`);
+        rl.close();
+      }
+    };
+    preguntarNota();
+  }); 
 }
-console.log(greeter("Elikat"));
+
+
+// Llamar a las funciones
+preguntarDatos();
+promedioNotas();
+
+
+
