1f20955a450ac20706095bed7fcbee64781031b1
elikat <205855200+EliKat2005@users.noreply.github.com>
2025-10-15 19:18:39 -0500

    Clase 15 oct

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+// Función para preguntar nombre y edad
 function preguntarDatos() {
   rl.question('¿Cuál es tu nombre? ', (nombre) => {
     rl.question('¿Cuál es tu edad? ', (edad) => {
@@ -23,6 +24,7 @@ function preguntarDatos() {
   });
 }
 
+// Función para calcular el promedio de notas
 function promedioNotas(){
   rl.question('¿Cuántas notas deseas ingresar? ', (cantidad) => {
     const numNotas = parseInt(cantidad);
@@ -58,10 +60,72 @@ function promedioNotas(){
   }); 
 }
 
+// Función para hacer preguntas de manera asincrónica
+function preguntaAsincrona(query: string): Promise<string> {
+  return new Promise((resolve) => {
+    rl.question(query, (respuesta) => {
+      resolve(respuesta);
+    });
+  });
+}
+
+// Función asincrónica para calcular el promedio de notas
+async function promNotas(){
+  try{
+    const cantidad = await preguntaAsincrona('¿Cuántas notas deseas ingresar? ');
+    const numNotas = parseInt(cantidad);
+    const notas: number[] = [];
+    let suma = 0;
+    let promedio = 0;
+
+    if (isNaN(numNotas) || numNotas <= 0) {
+      console.log('Por favor, ingresa un número válido mayor que cero.');
+      rl.close();
+      return;
+    }
+/* 
+    for (let i = 0; i < numNotas; i++) {
+      const notaStr = await preguntaAsincrona(`Ingresa la nota ${i + 1}: `);
+      const notaNumero = parseFloat(notaStr);
+      if (isNaN(notaNumero) || notaNumero < 0 || notaNumero > 10) {
+        console.log('Por favor, ingresa una nota válida entre 0 y 10.');
+        i--;
+      } else {
+        notas.push(notaNumero);
+      }
+    }
+      */   
+    while (notas.length < numNotas) {
+      const notaStr = await preguntaAsincrona(`Ingresa la nota ${notas.length + 1}: `);
+      const notaNumero = parseFloat(notaStr);
+      if (isNaN(notaNumero) || notaNumero < 0 || notaNumero > 10) {
+        console.log('Por favor, ingresa una nota válida entre 0 y 10.');
+      } else {
+        notas.push(notaNumero);
+      }
+    }
+
+    suma = notas.reduce((a, b) => a + b, 0);
+    promedio = suma / notas.length;
+
+    console.log(`El promedio de las notas es: ${promedio.toFixed(2)}`);
+    rl.close();
+  } catch(error){
+    console.error('Error:', error);
+    rl.close();
+  }
+}
+
+
+
+
+
 
 // Llamar a las funciones
-preguntarDatos();
-promedioNotas();
+
+promNotas();
+//preguntarDatos();
+//promedioNotas();
 
 
 
